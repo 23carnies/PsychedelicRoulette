2c169e555a307447d261a90aec04517f9df4f9e1
Karen Lobin Perkins <karen@Akula.attlocal.net>
2020-08-06 13:47:35 -0400

    First commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/*      Pseudocode      */
+// 1. set html, css, js basics
+// 2. set css numbers board
+// 3. set css circle for spinner
+//        multi colored wheel with numbers
+// 4. set play button
+// 5. set div for number to be appended to  
+// 6. set div for total
+// 7. set div for bet input field
+// 8. set div for prev numbers
+
+// set id for each section of square board
+// add event listener to wheel targeting id 
+
+// set transform-function rotate for wheel 
+//     --oh the math here
+//     https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function/rotate
+
+// set the base state of the board (start of game)
+//      no bets placed
+//      player has $1000
+
+//player can set bets in increments of $50
+
+//set function to randomly pic number 1-36
+
+//push prev numbers to list visible 
+
+
+
+// add voice of croupier Place your bets!, No more bets! number call outerHeight
+// add ticking sound of ball rolling
+
+
+//stretch goals::
+//      add ability to make 2 bets at once, one a number, one a section
+//      see the ball spin around the wheel
+
+
+
+
+
+
+
+
+// maybe not::
+
