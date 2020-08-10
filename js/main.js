4fbc7090a64158d6c29df376daff0930c87bb115
23carnies <46233990+23carnies@users.noreply.github.com>
2020-08-10 11:27:52 -0400

    Display player total 57957 functions. Wins/losses totals functions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 /*-------------Constants-------------*/
 const groupBets = {
-    sq37El: [1,4,7,10,13,16,19,22,25,28,31,34];
-    sq38El: [2,5,8,11,14,17,20,23,26,29,32,35];
-    sq39El: [3,6,9,12,15,18,21,24,27,30,33,36];
-    rec45Org: [2,4,6,8,10,11,13,14,15,17,20,22,23,24,26,28,29,31,33,35];
-    rec46Grn: [1,3,5,7,9,12,16,18,19,21,25,27,30,32,34,36];
+    sq37El: [1,4,7,10,13,16,19,22,25,28,31,34],
+    sq38El: [2,5,8,11,14,17,20,23,26,29,32,35],
+    sq39El: [3,6,9,12,15,18,21,24,27,30,33,36],
+    rec45Org: [2,4,6,8,10,11,13,14,15,17,20,22,23,24,26,28,29,31,33,35],
+    rec46Grn: [1,3,5,7,9,12,16,18,19,21,25,27,30,32,34,36]
 }
 
 
@@ -40,6 +40,9 @@ spinEl.addEventListener('click', render);
 
 
 /*-------------Functions-------------*/
+init();
+
+
 function init(){
     playerTotal = 1000;
     playerNumber = [];
@@ -47,6 +50,7 @@ function init(){
     //clear current bets
     //clear winning number div
     //clear prev winners
+    displayPlayerTotal();
 }
 
 function handleInsideBetsClick(e){
@@ -57,7 +61,7 @@ function handleInsideBetsClick(e){
 }
 function handleOutsideBetsClick(e){
     let recIndex = parseInt(e.target.id.replace('rec', ''));
-    console.log(recIndex);
+    //console.log(recIndex);
     playerOutsideBets.push(recIndex);
     return recIndex;
 }
@@ -70,7 +74,8 @@ function placeInsideBetChips(e){
     chip.className = 'chip'
     document.getElementById(insideId).appendChild(chip);
     displayBets.push(insideId);
-    console.log(insideId)
+    playerTotal -= 50;
+    console.log(displayBets)
     return insideId;
 }
 
@@ -83,27 +88,29 @@ function placeOustideBetChips(e){
     outChip.className = 'chip';
     document.getElementById(outsideId).appendChild(outChip);
     displayBets.push(outsideId);
-    console.log(outsideId);
+    playerTotal -= 50;
+    console.log(displayBets);
     return outsideId;
 }
 
-function determineTheBet(){
-    //for each chip placed on the board, total goes down 50
-    //maybe this is where i need an object finally
+function onBet(){
+    //for every chip that is put out, playerTotal should decrease by 50
 }
 
 function increaseBetOnSquare(){
 
 }
 
-function displayCurrentBets(insideId, outsideId){
-    currentBetEl.innerHTML = 'Current Bets</br>';
-    let displayBetsDiv = document.createElement('p');
-    let betsDisplay = document.createTextNode(`${insideId}, ${outsideId}`);
-    displayBetsDiv.appendChild(betsDisplay);
-    betsDisplay.className = 'displayNums';
-    currentBetEl.appendChild(betsDisplay);
-}
+// function displayCurrentBets(squareIndex, recIndex){
+//     currentBetEl.innerHTML = 'Current Bets</br>';
+//     let displayBetsDiv = document.createElement('p');
+//     let betsDisplay = document.createTextNode(`${squareIndex}, ${recIndex}`);
+//     displayBetsDiv.appendChild(betsDisplay);
+//     betsDisplay.className = 'displayNums';
+//     currentBetEl.appendChild(betsDisplay);
+//     //console.log(`${squareIndex}, ${recIndex}`)
+//     console.dir()
+// }
 
 function getWinningNumber(){
     winner = (Math.floor(Math.random()*37));
@@ -113,14 +120,14 @@ function getWinningNumber(){
 
 //still cant get the old number to leave before the new number arrives
 function displayWinningNum(){
-    winningNumDivEl.innerHTML = '';
-    let winNumPTag = document.createElement('p')
+    // winningNumDivEl.innerHTML = '';
     winNumPTag.innerHTML = '';
+    let winNumPTag = document.createElement('p')
     let winningNum = document.createTextNode(`${winner}`);
     winNumPTag.appendChild(winningNum);
     winNumPTag.className = 'winningNumber';
     document.getElementById('mainArt').appendChild(winNumPTag)
-    console.log(winNumPTag)
+    //console.log(winNumPTag)
 }
 
 //will need to figure how many to display and start .shift() to remove from beginning of array (oldest numbers)
@@ -132,53 +139,54 @@ function displayPreviousWinningNums(){
     prevNumPTag.appendChild(prevNumsDisp);
     prevNumsDisp.className = 'displayNums';
     prevNumsEl.appendChild(prevNumsDisp);
-    console.log(previousNums)
+    //console.log(previousNums)
 }
 
 /* not 100% about the math here. Will playerTotal add all 4 contingencies if necessary?*/
 function determineInsideWins(){
     if (playerInsideBets.includes(winner)){
-        playerTotal += (36 * betChip);
+        playerTotal += (36 * parseInt(betChip));
     }
     if (playerInsideBets.includes('37') && groupBets.sq37El.includes(winner)){
-        playerTotal += (3 * betChip);
+        playerTotal += (3 * parseInt(betChip));
     }
     if (playerInsideBets.includes('38') && groupBets.sq38El.includes(winner)){
-        playerTotal += (3 * betChip);
+        playerTotal += (3 * parseInt(betChip));
     }
     if (playerInsideBets.includes('39') && groupBets.sq39El.includes(winner)){
-        playerTotal += (3 * betChip);
+        playerTotal += (3 * parseInt(betChip));
     }
     return playerTotal;
+    console.log(playerInsideBets)
 }
 
 function determineOutsideWins(winner){
     if (playerOutsideBets.includes('40') && (winner >= 1 && winner <= 12)) {
-        playerTotal += (3 * betChip);
+        playerTotal += (3 * parseInt(betChip));
     }
     if (playerOutsideBets.includes('41') && (winner >= 13 && winner <=24)) {
-        playerTotal += (3 * betChip);
+        playerTotal += (3 * parseInt(betChip));
     }
     if (playerOutsideBets.includes('42') && (winner >= 25 && winner <= 36)) {
-        playerTotal += (3 * betChip);
+        playerTotal += (3 * parseInt(betChip));
     }
     if (playerOutsideBets.includes('43') && (winner >= 1 && winner <=18)) {
-        playerTotal += (2 * betChip);
+        playerTotal += (2 * parseInt(betChip));
     }
     if (playerOutsideBets.includes('44') && winner % 2 === 0) {
-        playerTotal += (2 * betChip);
+        playerTotal += (2 * parseInt(betChip));
     }
     if (playerOutsideBets.includes('45') && groupBets.rec45Org.includes(winner)) {
-        playerTotal += (2 * betChip);
+        playerTotal += (2 * parseInt(betChip));
     }
     if (playerOutsideBets.includes('46') && groupBets.rec46Grn.includes(winner)) {
-        playerTotal += (2 * betChip);
+        playerTotal += (2 * parseInt(betChip));
     }
     if (playerOutsideBets.includes('47') && winner % 2 !== 0) {
-        playerTotal += (2 * betChip);
+        playerTotal += (2 * parseInt(betChip));
     }
     if (playerOutsideBets.includes('48') && (winner >= 19 && winner <=36)) {
-        playerTotal += (2 * betChip);
+        playerTotal += (2 * parseInt(betChip));
     }
     return playerTotal;
 }
@@ -187,13 +195,22 @@ function determineLosses(){
 //something like if player inside/outside bets !== adding to total, then keep the money
 }
 
+function displayPlayerTotal() {
+    playerTotalEl.innerHTML = `Total $`;
+    let totalPTag = document.createElement('p');
+    let appTotal = document.createTextNode(`${playerTotal}`);
+    totalPTag.appendChild(appTotal);
+    appTotal.className = 'displaNums';
+    playerTotalEl.appendChild(appTotal);
+    console.log(appTotal);
+}
+
 function render(){
     getWinningNumber();
     determineInsideWins();
     determineOutsideWins();
     displayPreviousWinningNums();
-    let appTotal = document.createTextNode(`${playerTotal}`);
-    playerTotalEl.appendChild(appTotal)
+    displayPlayerTotal();
 }
 
 
