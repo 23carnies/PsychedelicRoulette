98588b765d16f15e9a02e7dfbc54eef9a76b4b30
23carnies <46233990+23carnies@users.noreply.github.com>
2020-08-11 15:57:40 -0400

    Update CSS. Add Cache ref elem/event listeners

---
+++ js/landing.js
@@ -0,0 +1,14 @@
+const hankEl = document.getElementById('hank');
+const dekeEl = document.getElementById('deke');
+const genevaEl = document.getElementById('geneva');
+const alexEl = document.getElementById('alex');
+
+hankEl.addEventListener('click', );
+dekeEl.addEventListener('click', );
+genevaEl.addEventListener('click', );
+alexEl.addEventListener('click', );
+
+function sayHello(){
+    
+}
+
