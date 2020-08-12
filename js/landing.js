a9e98b842b557f6fc00087ed7f7f4f68c42d22b5
23carnies <46233990+23carnies@users.noreply.github.com>
2020-08-12 16:09:56 -0400

    Add sound and link to landing page

--- js/landing.js
+++ js/landing.js
@@ -1,24 +1,29 @@
-const hankEl = document.getElementById('hank');
-const dekeEl = document.getElementById('deke');
-const genevaEl = document.getElementById('geneva');
-const alexEl = document.getElementById('alex');
-const closeWarningEl = document.getElementById('closeWarning');
-
+/* Warning Overlay */
+document.getElementById('closeWarning').addEventListener('click', closeWarning);
 window.addEventListener('load', openWarning);
-closeWarningEl.addEventListener('click', closeWarning);
-// hankEl.addEventListener('click', );
-// dekeEl.addEventListener('click', );
-// genevaEl.addEventListener('click', );
-// alexEl.addEventListener('click', );
-
-
 function openWarning(){
-    document.getElementById('overlay').style.dispaly = 'block';
+    document.getElementById('overlay').style.display = 'flex';
 }
 function closeWarning(){
     document.getElementById('overlay').style.display = 'none';
     document.getElementById('mainContent').style.display = 'flex';
 }
+
+/* Audio */
+const hank = new Audio('./audio/hank.mp3');
+const deke = new Audio('./audio/deke.mp3');
+const geneva = new Audio('./audio/geneva.mp3');
+const alex = new Audio('./audio/alexMedium.mp3');
+
+
+/* Choose A Persona */
+document.getElementById('hank').addEventListener('click', () => hank.play());
+document.getElementById('deke').addEventListener('click', () => deke.play());
+document.getElementById('geneva').addEventListener('click', () => geneva.play());
+document.getElementById('alex').addEventListener('click', () => alex.play());
+
+
+
 function sayHello(){
 
 }
